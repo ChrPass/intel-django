8bbc84e02e17d7a560f019a8d8f020350fec658f
ChrPass <xristospas97@gmail.com>
2022-09-25 20:26:26 +0300

    Corrections in post method and serializers

--- terms_api/serializers.py
+++ terms_api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Term, Synonyms
+import copy
 
 
 class TermSynonymSerializer(serializers.ModelSerializer):
@@ -9,22 +10,28 @@ class TermSynonymSerializer(serializers.ModelSerializer):
       model = Synonyms
       fields = ('id', 'name', 'scope', 'type')
 
-      def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.DATA, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED,
-                            headers=headers)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class TermSerializer(serializers.ModelSerializer):
     synonyms = TermSynonymSerializer(
         many=True,
     )
 
+    def create(self, request, *args, **kwargs):
+            data = copy.deepcopy(request)
+            syn_data = data.pop('synonyms')
+            term = Term.objects.create(**data)
+
+            for syn in syn_data:
+                Synonyms.objects.create(term=term,**syn)
+
+            return request
+            # if serializer.is_valid():
+            #     serializer.save()
+            #     headers = self.get_success_headers(serializer.data)
+            #     return Response(serializer.data, status=status.HTTP_201_CREATED,
+            #                     headers=headers)
+
+            # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     class Meta:
         model = Term
         fields = ["label", "has_children", "is_obsolete", "is_defining_ontology", 'is_defining_ontology',
@@ -32,3 +39,5 @@ class TermSerializer(serializers.ModelSerializer):
                   'description',
                   'lang',
                   'ontology_name', 'ontology_prefix', 'short_form', 'synonyms']
+        
+        
--- terms_api/views.py
+++ terms_api/views.py
@@ -16,25 +16,10 @@ class TermsListApiView(APIView):
     # for feature use
     def post(self, request, *args, **kwargs):
 
-        synonyms = request.data.get('synonyms')          
-
-        synSerializer = TermSynonymSerializer(data=synonyms)
-        data = {
-            'label': request.data.get('label'), 
-            'has_children': request.data.get('has_children'), 
-            'is_obsolete': request.data.get('is_obsolete'), 
-            'is_defining_ontology': request.data.get('is_defining_ontology'), 
-            'obo_id': request.data.get('obo_id'), 
-            'description': request.data.get('description'), 
-            'lang': request.data.get('lang'), 
-            'ontology_name': request.data.get('ontology_name'), 
-            'ontology_prefix': request.data.get('ontology_prefix'), 
-            'short_form': request.data.get('short_form'), 
-        }
-
-        serializer = TermSerializer(data=data)
+        serializer = TermSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
